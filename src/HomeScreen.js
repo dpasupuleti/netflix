ba474c93ad82d556d0313ac6cef2321ab128d64e
dpasupuleti <81898303+dpasupuleti@users.noreply.github.com>
2022-03-09 07:19:19 -0500

    navbar done

---
+++ src/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+const HomeScreen = () => {
+	return (
+		<div className='homeScreen'>
+			<Nav />
+			{/* banner */}
+			{/* Row */}
+		</div>
+	);
+};
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAUR_KmsdLflrBK6JVOoJO2Y1bmoeHEDsA",
+	authDomain: "netflix-67e76.firebaseapp.com",
+	projectId: "netflix-67e76",
+	storageBucket: "netflix-67e76.appspot.com",
+	messagingSenderId: "482309546119",
+	appId: "1:482309546119:web:b387c3c62863e141615822",
+};
