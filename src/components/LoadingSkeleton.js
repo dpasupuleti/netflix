f406be966ce25afd9977f8f380c17cb03efce8e7
dpasupuleti <81898303+dpasupuleti@users.noreply.github.com>
2022-03-13 01:04:20 -0500

    done!

--- src/components/LoadingSkeleton.js
+++ src/components/LoadingSkeleton.js
@@ -3,12 +3,8 @@ import "./LoadingSkeleton.css";
 const LoadingSkeleton = (props) => {
 	let counter = Math.floor(window.innerWidth / 100 / 2);
 	const classes = `skeleton ${props.type}`;
-	let temp;
-	for (let i = 1; i <= counter; i++) {
-		return (temp += i);
-	}
 	if (props.type === "thumbnail")
-		return Array(counter).fill(<div key={temp} className={classes}></div>);
+		return Array(counter).fill(<div className={classes}></div>);
 };
 
 export default LoadingSkeleton;
