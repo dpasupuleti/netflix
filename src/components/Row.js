f3c73e4bfc07bad74e3dcbf40ddec35b9dc5d806
dpasupuleti <81898303+dpasupuleti@users.noreply.github.com>
2022-03-14 00:07:00 -0400

    trailer popups done

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import movieTrailer from "movie-trailer";
+import Youtube from "react-youtube";
 import axios from "../axios";
 import LoadingSkeleton from "./LoadingSkeleton";
 import classes from "./Row.module.css";
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTraielrUrl] = useState();
 
 	const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -21,9 +24,32 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 		fetchData();
 		return () => setIsLoading(false);
 	}, [fetchUrl]);
+
+	const options = {
+		height: "390",
+		width: "100%",
+		playerVars: {
+			autoplay: 1,
+		},
+	};
+
+	const handleClick = (movie) => {
+		if (trailerUrl) {
+			setTraielrUrl("");
+		} else {
+			movieTrailer(movie?.name || "")
+				.then((url) => {
+					console.log(url);
+					const urlParams = new URLSearchParams(new URL(url).search);
+					setTraielrUrl(urlParams.get("v"));
+				})
+				.catch((e) => console.log(e));
+		}
+	};
+
 	return (
 		<div className={classes.row}>
-			<h2>{title}</h2>
+			<h2 style={{ paddingTop: "0.5rem" }}>{title}</h2>
 			<div className={classes.row__posters}>
 				{isLoading ? (
 					<LoadingSkeleton type='thumbnail' />
@@ -34,6 +60,7 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 								(!isLargeRow && movie.backdrop_path)) && (
 								<img
 									key={movie.id}
+									onClick={() => handleClick(movie)}
 									className={`${classes.row__poster} ${
 										isLargeRow && `${classes.row__posterLarge}`
 									}`}
@@ -46,6 +73,7 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 					)
 				)}
 			</div>
+			{trailerUrl && <Youtube videoId={trailerUrl} opts={options} />}
 		</div>
 	);
 };
