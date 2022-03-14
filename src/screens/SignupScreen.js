2f5ff7bff69c3a73e457d0eaf6c022c587d06e12
dpasupuleti <81898303+dpasupuleti@users.noreply.github.com>
2022-03-14 02:59:31 -0400

    error validation done!

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,34 +5,46 @@ import classes from "./SignupScreen.module.css";
 const SignupScreen = () => {
 	const [emailInput, setEmailInput] = useState("");
 	const [passwordInput, setPasswordInput] = useState("");
+	const [error, setError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 	const navigate = useNavigate();
-
 	const register = (e) => {
 		e.preventDefault();
-		auth
-			.createUserWithEmailAndPassword(emailInput, passwordInput)
-			.then((authUser) => {
-				if (authUser) {
-					navigate("/profile");
-					console.log(authUser);
-				}
-			})
-			.catch((e) => console.log(e.message));
+		if (emailInput && passwordInput) {
+			auth
+				.createUserWithEmailAndPassword(emailInput, passwordInput)
+				.then((authUser) => {
+					if (authUser) {
+						navigate("/profile");
+					}
+				})
+				.catch((e) => {
+					return setErrorMessage("Please enter valid email and Password");
+				});
+		} else {
+			setError(true);
+			setErrorMessage("please fill all the details");
+		}
 	};
 	const signIn = (e) => {
 		e.preventDefault();
-		auth
-			.signInWithEmailAndPassword(emailInput, passwordInput)
-			.then((authUser) => console.log(authUser))
-			.catch((e) => console.log(e.message));
+		if (emailInput && passwordInput) {
+			auth
+				.signInWithEmailAndPassword(emailInput, passwordInput)
+				.then((authUser) => console.log(authUser))
+				.catch((e) => {
+					return setErrorMessage("Incorrect email or Password");
+				});
+		} else {
+			setError(true);
+			setErrorMessage("please fill all the details");
+		}
 	};
 	const emailHandler = (e) => {
 		setEmailInput(e.target.value);
-		// console.log(e.target.value);
 	};
 	const passwordHandler = (e) => {
 		setPasswordInput(e.target.value);
-		// console.log(e.target.value);
 	};
 
 	return (
@@ -40,12 +52,14 @@ const SignupScreen = () => {
 			<form>
 				<h1>Sign In</h1>
 				<input
+					className={`${error && classes.inputField}`}
 					placeholder='Email or phone number'
 					onChange={emailHandler}
 					value={emailInput}
 					type='email'
 				/>
 				<input
+					className={`${error && classes.inputField}`}
 					onChange={passwordHandler}
 					placeholder='Password'
 					value={passwordInput}
@@ -55,6 +69,11 @@ const SignupScreen = () => {
 					Sign In
 				</button>
 				<h5 className={classes.signupScreen__gray}>Need help?</h5>
+				{error && (
+					<span className={classes.signupScreen__errorMessage}>
+						{errorMessage}
+					</span>
+				)}
 				<h4>
 					<span className={classes.signupScreen__gray}>New to Netflix? </span>
 					<span className={classes.signupScreen__link} onClick={register}>
